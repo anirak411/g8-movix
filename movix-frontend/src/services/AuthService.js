02e86222469f3aad2c363ad60be2c43a1c3d7c8d
Ed Lawrenz C. Portes <martyrslent@users.noreply.github.com>
2026-05-08 03:02:28 +0800

    Final build for SQAE testing2

--- movix-frontend/src/services/AuthService.js
+++ movix-frontend/src/services/AuthService.js
@@ -1,6 +1,6 @@
 // src/services/AuthService.js
 
-const API_BASE_URL = 'http://localhost:8081/api/auth'; // Ensure this matches your running Spring Boot server
+const API_BASE_URL = '/api/auth'; // Ensure this matches your running Spring Boot server
 
 // --- Register Function (POST /api/auth/register) ---
 export const register = async (email, password) => {
